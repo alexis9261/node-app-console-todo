6cfb8550022e886ccf196bb86fc9d00872301c7b
Alexis Montilla <alexisamm9261@gmail.com>
2022-08-04 10:56:57 -0400

    first commit - handle

---
+++ app.js
@@ -0,0 +1,22 @@
+require('colors')
+
+const { showMenu, pausa } = require('./helpers/messages')
+
+console.clear();
+const main = async () => {
+
+    let opt = '';
+
+    do {
+
+        opt = await showMenu();
+
+        console.log({opt});
+
+        if( opt !== '0' ) await pausa();
+
+    } while (opt !== '0');
+
+}
+
+main();
